5141aab409049424db277b68dd9814517bee149b
Adnan Ali <adnan.ali@gmail.com>
2012-06-24 17:52:37 -0400

    adding the home index page

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,12 @@ module CoolInnovations
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :view_specs    => false,
+        :routing_specs => false
+    end
   end
 end
