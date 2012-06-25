05f1976fea3fa1d611cae74ba99107a3055bd0ef
Adnan Ali <adnan.ali@gmail.com>
2012-06-25 10:21:47 -0400

    fix for #5

--- app/models/required_hardware.rb
+++ app/models/required_hardware.rb
@@ -1,7 +1,7 @@
 class RequiredHardware < ActiveRecord::Base
   attr_accessible :hardware_id, :part_id, :quantity
 
-  validates_presence_of :hardware_id, :part_id, :quantity
+  validates_presence_of :hardware_id, :quantity
   validates_uniqueness_of :hardware_id, :scope => :part_id
 
   belongs_to :hardware
