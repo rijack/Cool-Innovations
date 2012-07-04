b6d1d827a24002db0ff72a9df5e47d2f48623037
Adnan Ali <adnan.ali@gmail.com>
2012-07-04 13:34:39 -0400

    trying to put a tag inside a link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,14 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction, :search => params[:search]}, {:class => css_class}
-  end
 
+    i_class = "con-chevron-down"
+    if sort_direction == "asc"
+      i_class = "con-chevron-up"
+    end
+
+    link_to({:sort => column, :direction => direction, :search => params[:search]}, {:class => css_class}) do
+      tag(:i, :class=> i_class) + " " + title
+    end
+  end
 end
