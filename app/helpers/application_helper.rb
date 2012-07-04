5f2cec2e87faec627837cab94eeb17a9f270b2ce
Ron Ijack <ijack.ron@gmail.com>
2012-07-04 13:59:14 -0400

    added sorting icon and cleaning up search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,13 @@ module ApplicationHelper
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
 
-    i_class = "con-chevron-down"
+    i_class = "icon-chevron-down"
     if sort_direction == "asc"
-      i_class = "con-chevron-up"
+      i_class = "icon-chevron-up"
     end
 
     link_to({:sort => column, :direction => direction, :search => params[:search]}, {:class => css_class}) do
-      "#{content_tag(:i, "", :class=> i_class).html_safe if css_class} " + title
+      raw ("#{content_tag(:i, "", :class=> i_class).html_safe if css_class} " + title)
     end
   end
 end
