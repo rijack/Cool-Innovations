24e2a6c9deef86d6d952bf97cc83457f525c648a
Adnan Ali <adnan.ali@gmail.com>
2012-07-04 13:50:47 -0400

    i needs to close but not by itself.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
     end
 
     link_to({:sort => column, :direction => direction, :search => params[:search]}, {:class => css_class}) do
-      "#{tag(:i, :class=> i_class) if css_class} " + title
+      "#{content_tag(:i, "", :class=> i_class).safe_html if css_class} " + title
     end
   end
 end
