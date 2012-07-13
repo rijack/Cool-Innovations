7e88996d17c1ac93941a17a05eceac65481424a9
Ron Ijack <ijack.ron@gmail.com>
2012-07-13 11:21:41 -0400

    Added avatars to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email, :password, :username, :status, :name
+  attr_accessible :email, :password, :username, :status, :name, :avatar
 
   validates_presence_of :email, :username
   validates_presence_of :password, :on => :create
   validates_uniqueness_of :email, :username, :case_sensitive => false
 
   has_many :comments
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  #has_attached_file :avatar
 
   def admin?
     status == "admin"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120713135200) do
+ActiveRecord::Schema.define(:version => 20120713145720) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -128,10 +128,14 @@ ActiveRecord::Schema.define(:version => 20120713135200) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "type"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
-    t.string   "status",          :default => "user"
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.string   "status",              :default => "user"
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
