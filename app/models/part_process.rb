f59931e8dbf285247bef005b23b7357191fe16d3
Adnan Ali <adnan.ali@gmail.com>
2012-07-12 21:29:18 -0400

    show order_lines where that process is pending/in progress.

--- app/models/part_process.rb
+++ app/models/part_process.rb
@@ -9,4 +9,8 @@ class PartProcess < ActiveRecord::Base
 
   has_many :order_line_process_statuses
   has_many :order_lines, :through => :order_line_process_statuses
+
+  def order_lines_with_pending
+    order_lines.pending.where{(order_line_process_statuses.status == "in progress") | (order_line_process_statuses.status == "pending")}
+  end
 end
