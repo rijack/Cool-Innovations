03fb3d16c8ba99d4555077d61cbee995c3943a07
Ron Ijack <ijack.ron@gmail.com>
2012-07-19 11:19:59 -0400

    added default sort order for process order line

--- app/models/part_process.rb
+++ app/models/part_process.rb
@@ -12,6 +12,6 @@ class PartProcess < ActiveRecord::Base
   has_many :order_lines, :through => :order_line_process_statuses
 
   def order_lines_with_pending
-    order_lines.pending.where{(order_line_process_statuses.status == "in progress") | (order_line_process_statuses.status == "pending")}.order("order_line_process_statuses.order_line_priority asc")
+    order_lines.pending.where{(order_line_process_statuses.status == "in progress") | (order_line_process_statuses.status == "pending")}.order("order_line_process_statuses.order_line_priority asc").order("ship_date asc")
   end
 end
