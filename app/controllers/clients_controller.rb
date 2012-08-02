c66e13e8287ff8facd99fbd5610c8ff92ad3ef1e
Ron Ijack <ijack.ron@gmail.com>
2012-08-02 14:19:38 -0400

    added created date

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,10 +2,10 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.order(:name).page(params[:page]).per_page(20)
+    @clients = Client.order(:name).page(params[:page]).per_page(30)
 
-    params[:direction] ||= "asc"
-    params[:sort] ||= "due_date"
+    #params[:direction] ||= "asc"
+    #params[:sort] ||= "due_date"
 
     respond_to do |format|
       format.html # index.html.erb
